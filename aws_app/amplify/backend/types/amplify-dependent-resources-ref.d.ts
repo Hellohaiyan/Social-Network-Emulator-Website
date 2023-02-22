c55a66475b714666f9976c91aafcdf9cc6635dd8
Satjyot Atwal <satjyotatwal@gmail.com>
2023-02-22 00:54:18 -0800

    Added sne DynamoDB, REST API, and Lambda functions

--- aws_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ aws_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,28 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "sneRESTAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "sneLambdaFun": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "sneDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
