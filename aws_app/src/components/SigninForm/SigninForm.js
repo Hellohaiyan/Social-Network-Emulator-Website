5b3464b46c740cfdd0dafb5892f05a926ea05518
Haiyan Liao <finished lab4>
2023-02-24 23:11:28 -0800

    Add folders under src

---
+++ aws_app/src/components/SigninForm/SigninForm.js
@@ -0,0 +1,22 @@
+export function SigninForm() 
+{
+    return (
+      <div className="login-wrapper">
+        <h1>Please Sign In</h1>
+        <form id = "form_login">
+          <label>
+            <p>Email</p>
+            <input type="text" />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" />
+          </label>
+          <div>
+            <button type="submits">Sign in</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+  
