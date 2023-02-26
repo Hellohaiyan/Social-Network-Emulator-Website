7cca0ae11f9be2ddb8d8105889f7ded083ba5ac5
Satjyot Atwal <satjyotatwal@gmail.com>
2023-02-26 14:48:06 -0800

    Setting up new project

--- aws_app/src/App.js
+++ aws_app/src/App.js
@@ -2,20 +2,9 @@ import './App.css';
 import React from 'react';
 import {SignupForm} from './components/SignupForm/SignUp.js';
 import {SigninForm} from './components/SigninForm/SigninForm.js';
-// const axios = require('axios');
-import axios from 'axios'
 
 function App() 
 {
-  axios.get('https://x6cagtyd32.execute-api.us-west-1.amazonaws.com/users')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
   return (
     <section>
       <SignupForm/>
