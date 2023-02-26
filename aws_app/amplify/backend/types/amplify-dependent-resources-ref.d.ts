49ff6ecac3a0d67ff2589f589ab0d794db64a69f
Satjyot Atwal <satjyotatwal@gmail.com>
2023-02-26 02:27:59 -0800

    Created HTTP API for sne

--- aws_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ aws_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "sneHTTPAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "function": {
     "sneUsersFun": {
       "Arn": "string",
