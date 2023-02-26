8ccd1fe52648e5a3a3bd13ec80a186066b44c80c
Satjyot Atwal <satjyotatwal@gmail.com>
2023-02-26 15:24:53 -0800

    Used axios to make api calls

--- aws_app/src/App.js
+++ aws_app/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import React from 'react';
 import {SignupForm} from './components/SignupForm/SignUp.js';
 import {SigninForm} from './components/SigninForm/SigninForm.js';
+import axios from 'axios';
 
 function App() 
 {
+  axios('https://agx9exeaue.execute-api.us-west-1.amazonaws.com/users').then(function (res) {
+    console.log(res)
+  });
   return (
     <section>
       <SignupForm/>
