0a6cc10bc050f0a224fb7373930735e3d6b3224f
Satjyot Atwal <satjyotatwal@gmail.com>
2023-02-26 16:23:27 -0800

    Setup PDS table, function, role, and api

--- aws_app/src/App.js
+++ aws_app/src/App.js
@@ -9,6 +9,10 @@ function App()
   axios('https://agx9exeaue.execute-api.us-west-1.amazonaws.com/users').then(function (res) {
     console.log(res)
   });
+  axios('https://u4gaaf1f07.execute-api.us-west-1.amazonaws.com/users').then(function (res) {
+    console.log(res)
+  });
+
   return (
     <section>
       <SignupForm/>
